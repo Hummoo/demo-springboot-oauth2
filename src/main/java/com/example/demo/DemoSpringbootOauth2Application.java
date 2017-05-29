de8c888f1fdcd51c41894e3c01f8abafd6b68335
Tagdanai Sujinnapram <hummoosr17@gmail.com>
2017-05-29 17:18:26 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/DemoSpringbootOauth2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootOauth2Application.class, args);
+	}
+}
